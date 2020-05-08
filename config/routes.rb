3af555d21757fa1195480d374518f4c558015bac
Henry-Harcourt <hnry.harcourt@gmail.com>
2020-05-08 13:11:21 +1200

    added styles and post model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root route / landing page
+  root 'posts#index', as: 'home'
+
+  # regular route
+  get 'about' => 'pages#about', as: 'about'
+
+  # this auto generates a bunch of routes that we will use to create, edit, delete posts etc
+  resources :posts
 end
